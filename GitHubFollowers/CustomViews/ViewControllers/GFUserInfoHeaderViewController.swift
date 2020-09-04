288cbe9b04182bc5d56527a43ad7b0df102af1ce
Diego Benites Lopes <dbeniteslopes@gmail.com>
2020-09-04 16:37:50 -0300

    Refactoring to avoid code duplication

--- GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
+++ GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
@@ -29,7 +29,7 @@ class GFUserInfoHeaderViewController: UIViewController {
     }
     
     func configureUIElements() {
-        downloadAvatarImage()        
+        avatarImageView.downloadImage(fromURL: user.avatarUrl)
         usernameLabel.text = user.login
         nameLabel.text = user.name ?? ""
         locationLabel.text = user.location ?? "No Location"
@@ -39,17 +39,7 @@ class GFUserInfoHeaderViewController: UIViewController {
         locationImageView.image = SFSymbols.location
         locationImageView.tintColor = .secondaryLabel
     }
-    
-    private func downloadAvatarImage() {
-        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] (image) in
-            guard let self = self else { return }
-            
-            DispatchQueue.main.async {
-                self.avatarImageView.image = image
-            }
-        }
-    }
-    
+
     func addSubviews() {
         view.addSubview(avatarImageView)
         view.addSubview(usernameLabel)
